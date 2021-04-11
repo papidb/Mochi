20962f1e17af72296a36c187f9327d87092abacc
Daniel Benjamin <benjamindaniel706@gmail.com>
2021-04-11 16:45:13 +0100

    Rectify refresh token used as token

--- src/helpers/passport.js
+++ src/helpers/passport.js
@@ -1,26 +1,30 @@
+import httpStatus from 'http-status';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 import config from '../config';
+import tokenTypes from '../config/tokens';
 import Models from '../models';
+import ApiError from '../utils/ApiError';
 const { User } = Models;
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.jwt.secret;
 
-const jwtStrategy = new JwtStrategy(opts, async function (jwt_payload, done) {
-  return await User.findOne({ id: jwt_payload.sub })
-    .then(user => {
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-        // or you could create a new account
-      }
-    })
-    .catch(err => {
-      return done(err, false);
-    });
-});
+const jwtVerify = async function (jwt_payload, done) {
+  try {
+    if (jwt_payload.type !== tokenTypes.ACCESS) {
+      throw new ApiError(httpStatus.UNAUTHORIZED, 'Invalid token type');
+    }
+    const user = await User.findOne({ id: jwt_payload.sub });
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+};
+const jwtStrategy = new JwtStrategy(opts, jwtVerify);
 
 export default jwtStrategy;
